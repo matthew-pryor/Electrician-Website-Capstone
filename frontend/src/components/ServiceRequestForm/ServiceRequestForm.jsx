8eac02765ac8ac359cadf4db2c4651e2ec88047f
matthew-pryor <pryor.matthew.a@gmail.com>
2022-04-08 16:14:30 -0500

    fixed an error on line 42 in the servicerequestform where a space was required after the Bearer in order for the post request to be authorized

--- frontend/src/components/ServiceRequestForm/ServiceRequestForm.jsx
+++ frontend/src/components/ServiceRequestForm/ServiceRequestForm.jsx
@@ -13,15 +13,13 @@ const ServiceRequestForm = (props) => {
     const [zip_code, setZipCode] = useState('');
     const [phone_number, setPhoneNumber] = useState('');
     const [email_address, setEmailAddress] = useState('');
-    const [electrician_id, setElectricianId] = useState('');
     const [appointment_date_time, setAppointmentDateTime] = useState('');
-    const [service_id, setServiceId] = useState('');
     const [description, setDescription] = useState('');
 
     async function handleSubmitForm(event) {
         event.preventDefault();
         let newRequest = {
-            user_id: user.username,
+            user_id: user.id,
             first_name: first_name,
             last_name: last_name,
             address: address,
@@ -30,10 +28,10 @@ const ServiceRequestForm = (props) => {
             zip_code: zip_code,
             phone_number: phone_number,
             email_address: email_address,
-            electrician_id: electrician_id,
+            electrician_id: 1,
             appointment_date_time: appointment_date_time,
-            appointment_status: 5,
-            service_id: service_id,
+            appointment_status_id: 5,
+            service_id: 1,
             description: description
 
         };
@@ -41,7 +39,7 @@ const ServiceRequestForm = (props) => {
         console.log(newRequest);
         await axios.post('http://127.0.0.1:8000/api/appointments/', newRequest, {
             headers: {
-                Authorization: "Bearer" + token,
+                Authorization: "Bearer " + token,
             }
         })
     };
@@ -49,28 +47,62 @@ const ServiceRequestForm = (props) => {
 
     return ( 
         <form onSubmit={handleSubmitForm}>
-            <h1>Schedule and Appointment:</h1>
+            <h1>Schedule an Appointment:</h1>
+
+            <div>
             <label>First Name</label>
             <input type='text' placeholder="First Name..." value={first_name} onChange={(event) => setFirstName(event.target.value)}/>
+            </div>
+
+            <div>
             <label>Last Name</label>
             <input type='text' placeholder="Last Name..." value={last_name} onChange={(event) => setLastName(event.target.value)}/>
+            </div>
+
+            <div>
             <label>Address</label>
-            <input type='text' placeholder="Address..." value={address} onChange={(event) => setAdderss(event.target.value)}/>
+            <input type='text' placeholder="Address..." value={address} onChange={(event) => setAdderss(event.target.value)}/>  
+            </div>
+
+            <div>
             <label>City</label>
             <input type='text' placeholder="City..." value={city} onChange={(event) => setCity(event.target.value)}/>
+            </div>
+
+            <div>
             <label>State</label>
-            <input type='text' placeholder="State..." value={state} onChange={(event) => setState(event.target.value)}/>
+            <input type='text' placeholder="State..." value={state} onChange={(event) => setState(event.target.value)}/>   
+            </div>
+
+            <div>
             <label>Zip Code</label>
             <input type='text' placeholder="XXXXX" value={zip_code} onChange={(event) => setZipCode(event.target.value)}/>
+            </div>
+
+            <div>
             <label>Phone Number</label>
             <input type='text' placeholder="XXX-XXX-XXXX" value={phone_number} onChange={(event) => setPhoneNumber(event.target.value)}/>
+            </div>
+
+            <div>
             <label>Email Address</label>
             <input type='text' placeholder="XXXX@email.com" value={email_address} onChange={(event) => setEmailAddress(event.target.value)}/>
+            </div>
+
+            <div>
             <label>Date/Time</label>
-            <input type='text' placeholder="YYYY-MM-DD" value={appointment_date_time} onChange={(event) => setAppointmentDateTime(event.target.value)}/>
+            <input type='text' placeholder="YYYY-MM-DD" value={appointment_date_time} onChange={(event) => setAppointmentDateTime(event.target.value)}/> 
+            </div>
+
+            <div>
             <label>Description</label>
             <input type='text' placeholder="Please provide as many details about the service you are requesting as possible" value={description} onChange={(event) => setDescription(event.target.value)}/>
-            <button type="text" class="submit">Add Song</button>
+            </div>
+
+            <div>
+            <button type="text" class="submit">Submit</button>
+            </div>
+
         </form>
      );
 }
--- frontend/src/pages/ServiceRequestPage/ServiceRequestPage.js
+++ frontend/src/pages/ServiceRequestPage/ServiceRequestPage.js
@@ -1,41 +1,16 @@
 import React from "react";
-import { useEffect, useState } from "react";
-
-import axios from "axios";
 import useAuth from "../../hooks/useAuth";
-import DemoApp from "../../components/Calendar/Calendar";
+import ServiceRequestForm from "../../components/ServiceRequestForm/ServiceRequestForm";
 
 const ServiceRequestPage = () => {
   // The "user" value from this Hook contains the decoded logged in user information (username, first name, id)
   // The "token" value is the JWT token that you will send in the header of any request requiring authentication
   const [user, token] = useAuth();
-  const [cars, setCars] = useState([]);
 
-  useEffect(() => {
-    const fetchCars = async () => {
-      try {
-        let response = await axios.get("http://127.0.0.1:8000/api/cars/", {
-          headers: {
-            Authorization: "Bearer " + token,
-          },
-        });
-        setCars(response.data);
-      } catch (error) {
-        console.log(error.message);
-      }
-    };
-    fetchCars();
-  }, [token]);
   return (
     <div className="container">
-      <h1>Home Page for {user.username}!</h1>
-      {cars &&
-        cars.map((car) => (
-          <p key={car.id}>
-            {car.year} {car.model} {car.make}
-          </p>
-        ))}
-        <DemoApp/>
+      <h1>Request Services Page</h1>
+        <ServiceRequestForm/>
     </div>
   );
 };
