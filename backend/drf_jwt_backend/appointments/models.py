4fbd8519c1942312f64f8583e69e7a98b6d41679
matthew-pryor <pryor.matthew.a@gmail.com>
2022-04-11 14:59:33 -0500

    nuked the database

--- backend/drf_jwt_backend/appointments/models.py
+++ backend/drf_jwt_backend/appointments/models.py
@@ -1,3 +1,4 @@
+from turtle import title
 from django.db import models
 from django.contrib.auth.models import User
 from appointment_status.models import AppointmentStatus
@@ -16,10 +17,14 @@ class Appointment(models.Model):
     zip_code = models.CharField(max_length=255)
     phone_number = models.CharField(max_length=255)
     email_address = models.CharField(max_length=255)
-    appointment_date_time = models.CharField(max_length=255)
+    title = models.CharField(max_length=20)
+    start = models.DateField()
+    end = models.DateField()
     appointment_status = models.ForeignKey(AppointmentStatus, on_delete=models.CASCADE)
     service = models.ForeignKey(Service, on_delete=models.CASCADE)
     description = models.CharField(max_length=3000)
     electrician_id = models.IntegerField()
+
+    # "2022-04-27"
 
 
--- backend/drf_jwt_backend/appointments/serializers.py
+++ backend/drf_jwt_backend/appointments/serializers.py
@@ -4,7 +4,7 @@ from .models import Appointment
 class AppointmentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Appointment
-        fields = ['id', 'user_id', 'first_name', 'last_name', 'address', 'city', 'state', 'zip_code', 'phone_number', 'email_address', 'electrician_id', 'appointment_date_time', 'appointment_status', 'appointment_status_id', 'service', 'service_id', 'description']
+        fields = ['id', 'user_id', 'first_name', 'last_name', 'address', 'city', 'state', 'zip_code', 'phone_number', 'email_address', 'electrician_id', 'title', 'start', 'end', 'appointment_status', 'appointment_status_id', 'service', 'service_id', 'description']
         depth = 1
 
     appointment_status_id = serializers.IntegerField(write_only=True)
