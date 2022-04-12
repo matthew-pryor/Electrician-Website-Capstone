0cef035fe880a39bc5c8c7bc84f1e86b75fa9f07
matthew-pryor <pryor.matthew.a@gmail.com>
2022-04-12 16:18:23 -0500

    adjusted calendar properties to make certain times and days uninteractable to the user + working on converting a time model field to be usable in the fullcalendar calendar component so they can be displayed properly

--- frontend1/src/components/Calendar/Calendar.jsx
+++ frontend1/src/components/Calendar/Calendar.jsx
@@ -13,7 +13,7 @@ const Calendar = (props) => {
   useEffect(()=>{
     const fetchEvents = async () => {
       try {
-        let response = await axios.get();
+        let response = await axios.get('http://127.0.0.1:8000/api/appointments/all/');
         setEvents(response.data);
       }
       catch(error){
@@ -27,23 +27,35 @@ const Calendar = (props) => {
   <div>
     <div>
 
-      <div>
-        <Link to="/"><button>Confirm</button></Link>
-      </div>
-
       <FullCalendar
       plugins = {[dayGridPlugin, timeGridPlugin, interactionPlugin]}
       headerToolbar={{
         left: 'prev, next today',
         center: 'title',
         right: 'dayGridMonth, timeGridWeek, timeGridDay'
+        
       }}
       initialView='dayGridMonth'
       editable={true}
       selectable={true}
       selectMirror={true}
       events={events}
+      selectOverlap={false}
+      navLinks={true}
+      navLinkDayClick={true}
+      businessHours={true}
+      selectConstraint={'businessHours'}
+      eventClick={
+        function(arg) {
+          alert(arg.event.title)
+          alert(arg.event.start)    
+        }
+      }
       />
+
+      <div>
+        <Link to="/"><button>Confirm</button></Link>
+      </div>
 
     </div>
 
