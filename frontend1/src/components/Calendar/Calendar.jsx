27f3f91ea279e615c8d0eb3d08ac2ab3016a8692
matthew-pryor <pryor.matthew.a@gmail.com>
2022-04-12 11:09:00 -0500

    installed necessary fullcalendar packages

--- frontend1/src/components/Calendar/Calendar.jsx
+++ frontend1/src/components/Calendar/Calendar.jsx
@@ -3,6 +3,13 @@ import FullCalendar from '@fullcalendar/react' // must go before plugins
 import dayGridPlugin from '@fullcalendar/daygrid' // a plugin!
 
 export default class DemoApp extends React.Component {
+
+  state = {
+    weekendsVisible: true,
+    currentEvents: []
+  }
+
+
   render() {
     return (
       <FullCalendar
