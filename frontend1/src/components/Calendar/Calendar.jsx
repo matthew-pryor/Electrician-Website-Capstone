11cf2feaa3812b8d1a6ece21bde9213e03bf18fb
matthew-pryor <pryor.matthew.a@gmail.com>
2022-04-12 14:06:05 -0500

    fleshed out the calendar component and tested it, needs the url for the async function entered

--- frontend1/src/components/Calendar/Calendar.jsx
+++ frontend1/src/components/Calendar/Calendar.jsx
@@ -4,53 +4,50 @@ import dayGridPlugin from '@fullcalendar/daygrid'
 import timeGridPlugin from '@fullcalendar/timegrid'
 import interactionPlugin from '@fullcalendar/interaction'
 import axios from 'axios'
-
 import { Link } from "react-router-dom";
 
-const Calendar = () => {
+const Calendar = (props) => {
 
- const [events, setEvents] = useState([]);
+  const [events, setEvents] = useState([]);
 
-    useEffect(() => {
-        
-      const fetchEvents = async () => {
-          try {
-              let response = await axios.get("http://127.0.0.1:8000/api/appointments/events/"); //Data from Database unprotected
-              setEvents(response.data);
-            } catch (error) {
-              console.log(error.message);
-            }
-          };
-        fetchEvents();
-        console.log("EVENT DATA:", events)
-      }, []);
-      
-      
-      
-      return ( 
-        <div className="calendar">
-            <div className='calendar-main'>
-              <div className='back-home'>
-                <Link to="/"><button>Back to Home</button></Link>
-              </div>
-                <FullCalendar
-                plugins={[dayGridPlugin,timeGridPlugin,interactionPlugin]}
-                headerToolbar={{
-                  left: 'prev,next today',
-                  center: 'title',
-                  right: 'dayGridMonth,timeGridWeek,timeGridDay'
-                }}
-                initialView='dayGridMonth'
-                editable={true}
-                selectable={true}
-                selectMirror={true}
-                events={events}/>
-               
+  useEffect(()=>{
+    const fetchEvents = async () => {
+      try {
+        let response = await axios.get();
+        setEvents(response.data);
+      }
+      catch(error){
+        console.log(error.message)
+      }
+    }
+    fetchEvents();
+  }, []);
 
-            </div>
-        </div>
+  return (
+  <div>
+    <div>
 
-);
+      <div>
+        <Link to="/"><button>Confirm</button></Link>
+      </div>
+
+      <FullCalendar
+      plugins = {[dayGridPlugin, timeGridPlugin, interactionPlugin]}
+      headerToolbar={{
+        left: 'prev, next today',
+        center: 'title',
+        right: 'dayGridMonth, timeGridWeek, timeGridDay'
+      }}
+      initialView='dayGridMonth'
+      editable={true}
+      selectable={true}
+      selectMirror={true}
+      events={events}
+      />
+
+    </div>
+
+  </div>);
 }
 
 export default Calendar;
