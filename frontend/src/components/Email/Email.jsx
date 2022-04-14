a01589572dce89529cfc9ab249ccb2486aa70576
matthew-pryor <pryor.matthew.a@gmail.com>
2022-04-14 11:39:49 -0500

    created forms for the contact portion for the customer + running into an error regarding an unsupported fs module, needs reseach/troubleshooting

--- frontend/src/components/Email/Email.jsx
+++ frontend/src/components/Email/Email.jsx
@@ -1,14 +1,75 @@
 import React from "react";
-import {Row, Col} from 'antd';
+import {Row, Col, Form, Button, Space, Input} from 'antd';
 import 'antd/dist/antd.css'
 
 const Email = (props) => {
 
+    const [form] = Form.useForm();
+    const {TextArea} = Input;
+    const sgMail = require('@sendgrid/mail');
+    sgMail.setApiKey(
+        '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'
+    );
 
+    const onComplete = (fields) => {
+        const message = {
+            to: 'pocavo@gmail.com',
+            from: fields.email,
+            subject: fields.subject,
+            html: `
+            <p><strong>Name:</strong> ${fields.name}</p>
+            <p>${fields.message}</p>
+            `
+        }
+        
+        sgMail
+        .send(message)
+        .then(()=>{
+            form.resetFields();
+            console.log('Email Sent!');
+        })
+        .catch((error)=>{
+            console.error('Error: ', error)
+        });
+    }
 
     return ( 
         <Row gutter={24} style={{padding: '30px'}}>
-            <Col xl={12}>Hello</Col>
+            <Col xl={12}>
+                <Form layout="vertical" form={form} onFinish={onComplete}>
+
+                    <Form.Item name='name' label='Name' rules={[{required: true}]}>
+                        <Input/>
+                    </Form.Item>
+
+                    <Form.Item name='email' label='Email' rules={[{required: true}]}>
+                        <Input/>
+                    </Form.Item>
+
+                    <Form.Item name='phone' label='Phone Number' rules={[{required: true}]}>
+                        <Input/>
+                    </Form.Item>
+
+                    <Form.Item name='date' label='Requested Date for Services' rules={[{required: true}]}>
+                        <Input/>
+                    </Form.Item>
+
+                    <Form.Item name='subject' label='Subject' rules={[{required: true}]}>
+                        <Input/>
+                    </Form.Item>
+
+                    <Form.Item name='descripition' label='Description of Service' rules={[{required: true}]}>
+                        <TextArea/>
+                    </Form.Item>
+
+                    <Form.Item>
+                        <Space>
+                            <Button type='primary' htmlType="submit">Submit</Button>
+                            <Button type='secondary' htmlType="submit" onClick={(e)=>form.resetFields()}>Clear</Button>
+                        </Space>
+                    </Form.Item>
+                </Form>
+            </Col>
         </Row>
      );
 }
