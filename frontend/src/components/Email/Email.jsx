3d6c360e4ab48e1dc23e8a6a6dfd12c69d6a89ba
matthew-pryor <pryor.matthew.a@gmail.com>
2022-04-14 10:45:39 -0500

    renamed the frontend1 folder to frontend + initialized the email component to work with the email API + installed email API packages

---
+++ frontend/src/components/Email/Email.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import {row, Col} from 'antd';
+import 'antd/dist/antd.css'
+
+const Email = (props) => {
+
+
+
+    return ( 
+        <Row>
+            
+        </Row>
+     );
+}
+ 
+export default Email;
