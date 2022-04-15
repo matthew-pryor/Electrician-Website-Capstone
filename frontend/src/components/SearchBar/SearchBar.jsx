4152d0e0643246276dd0026d86ee229d79510266
matthew-pryor <pryor.matthew.a@gmail.com>
2022-04-15 15:48:40 -0500

    created a search bar for the electricians page and instantiated it + successfully tested its functionality

---
+++ frontend/src/components/SearchBar/SearchBar.jsx
@@ -0,0 +1,33 @@
+import React, {useState} from "react";
+import "./SearchBar.css"
+
+const SearchBar = (props) => {
+
+    const [searchElectricians, setSearchElectrians] = useState('');
+
+    function handleSubmit(entry){
+        entry.preventDefault();
+        props.filterElectricians(searchElectricians)
+        setSearchElectrians('')
+    }
+
+    return ( 
+        <div className="searchbar-container"> 
+            <form onSubmit={handleSubmit}>
+                    <input 
+                    value={searchElectricians} 
+                    onChange={(entry) => setSearchElectrians(entry.target.value)} 
+                    type="text" 
+                    placeholder=" Search by Name, City, Zip Code or Services..." 
+                    className="input"></input>
+                    <button type="text" class="submit" className="search-button">Search</button>
+            </form>
+        </div>
+                
+                
+                
+
+     );
+}
+ 
+export default SearchBar;
--- frontend/src/pages/ElectriciansPage/ElectriciansPage.js
+++ frontend/src/pages/ElectriciansPage/ElectriciansPage.js
@@ -1,13 +1,15 @@
 import React, { useEffect, useState } from "react";
 import useAuth from "../../hooks/useAuth";
 import axios from "axios";
-import { useParams } from "react-router-dom";
 import DisplayElectricians from "../../components/DisplayElectricians/DisplayElectricians";
+import SearchBar from "../../components/SearchBar/SearchBar";
 
 const ElectriciansPage = () => {
   // The "user" value from this Hook contains the decoded logged in user information (username, first name, id)
   // The "token" value is the JWT token that you will send in the header of any request requiring authentication
-  const [electricians, setElectricians] = useState('');
+  const [electricians, setElectricians] = useState([]);
+  const [displayedElectricians, setDisplayedElectricians] = useState([]);
+  const [search, setSearch] = useState('')
 
   useEffect(() => {
     getAllElectricians();
@@ -18,10 +20,28 @@ const ElectriciansPage = () => {
       setElectricians(response.data);
   }
 
+  const filterElectricians = (searchElectrician) => {
+      let matchingElectricians = electricians.filter((electrician)=>{
+          if (electrician.name.toLowerCase().includes(searchElectrician.toLowerCase()) || 
+          electrician.city.toLowerCase().includes(searchElectrician.toLowerCase()) || 
+          electrician.zip_code.includes(searchElectrician) ||
+          electrician.services.toLowerCase().includes(searchElectrician.toLowerCase())){
+              return true;
+          }
+          else{
+              return false;
+          }
+      })
+      setSearch(searchElectrician)
+      setDisplayedElectricians(matchingElectricians)
+  }
+
   return (
     <div className="container">
 
-    <DisplayElectricians electricians={electricians}/>
+    <SearchBar filterElectricians={filterElectricians}/>
+
+    <DisplayElectricians electricians={displayedElectricians} search={search}/>
 
     </div>
   );
